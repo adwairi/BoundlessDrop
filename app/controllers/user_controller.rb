2040f134ccf707df70b8afa8d2da4f43f4bbd660
Ali Dwairi <adwairi88@gmail.com>
2017-06-22 13:18:54 +0300

    - create survey - add questions

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,29 +1,4 @@
 class UserController < ApplicationController
   before_action :authenticate_user!
-  def show
-    @id = current_user.id
-    @user = User.find(@id)
-    @user.valid?
-  end
-
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
-    else
-      render 'new'
-    end
-  end
-
-
-  private
-  def user_params
-      params.require(:user).permit(:first_name, :last_name, :username, :email, :password)
-  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  include ActiveModel::Validations
-  # validates_with EmailValidator
-  #
-  # validates :first_name, presence: true, length: { minimum: 3 }
-  # validates :last_name, presence: true, length: { minimum: 3 }
-  # validates :username, presence: true, length: { minimum: 5 }, uniqueness: {scope: :username, message: "username is already taken"}
 
 end
