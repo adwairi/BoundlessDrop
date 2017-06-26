6b6e0f3b27e36ddc62d2eaf59eb988813157849a
Ali Dwairi <adwairi88@gmail.com>
2017-06-26 04:53:59 +0300

    answer survey

--- app/controllers/answer_controller.rb
+++ app/controllers/answer_controller.rb
@@ -6,16 +6,23 @@ class AnswerController < ApplicationController
 
   def create
     @answers_param = params[:answer]
+
     if !@answers_param.blank?
       @answer_temp = @answers_param.first
       @answer_temp[1].each do |question|
-        @answer = SurveyAnswer.new
-        @answer.survey_id = @answer_temp[0]
-        @answer.question_id = question[0]
-        @answer.answer = question[1]
-        @answer.save
+        if question[1].strip != ''
+          @answer = SurveyAnswer.new
+          @answer.survey_id = @answer_temp[0]
+          @answer.question_id = question[0]
+          @answer.answer = question[1]
+          @answer.save
+        end
       end
     end
+  end
+
+  def success
+    @x = 'ssssssssssss'
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,29 +4,27 @@ Rails.application.routes.draw do
   resources :answer
 
   get 'question/add_question'
-
-  get 'survey/edit'
-
-  get 'survey/delete'
-  delete 'survey/delete/:id', to: 'survey#delete'
-
-  get 'survey/show'
-  get 'survey/show/:id', to: 'survey#show'
-
-  post 'survey/publish', to: 'survey#publish'
-
-  get 'survey/index'
-  get 'survey/', to: 'survey#index'
-
-  get 'online-survey', to: 'answer#start'
-  post 'online-survey', to: 'answer#create'
-
-  get 'survey/add_survey'
-  post 'survey/save_survey'
-
   get 'question/add_question'
   post 'question/save_question'
   delete 'question/delete/:id', to: 'question#delete'
+
+  get 'survey/index'
+  get 'survey/', to: 'survey#index'
+  get 'survey/edit'
+  get 'survey/add_survey'
+  get 'survey/show'
+  get 'survey/show/:id', to: 'survey#show'
+  get 'survey/delete'
+  delete 'survey/delete/:id', to: 'survey#delete'
+  post 'survey/save_survey'
+  post 'survey/publish', to: 'survey#publish'
+
+
+  get 'online-survey', to: 'answer#start'
+  get 'online-survey/success', to: 'answer#success'
+  post 'online-survey', to: 'answer#create'
+
+
 
 
   devise_for :user
